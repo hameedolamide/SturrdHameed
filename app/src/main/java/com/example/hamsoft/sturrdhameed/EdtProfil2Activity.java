ebabad8190223dfba31bca3f2e9da5ded774774e
Atobatele Hameed <hakmid4united@gmail.com>
2020-01-06 00:30:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/hamsoft/sturrdhameed/EdtProfil2Activity.java
@@ -0,0 +1,13 @@
+package com.example.hamsoft.sturrdhameed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EdtProfil2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edt_profil2);
+    }
+}
---
+++ app/src/main/java/com/example/hamsoft/sturrdhameed/EdtProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.hamsoft.sturrdhameed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EdtProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edt_profile);
+    }
+}
